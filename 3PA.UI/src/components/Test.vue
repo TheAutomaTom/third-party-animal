4213a24d90004b3b72f0aeb719942fad5e018bd9
TheAutomaTom <TheAutomaTom@Gmail.com>
2022-02-01 10:49:33 -0500

    Add Vue project

---
+++ 3PA.UI/src/components/Test.vue
@@ -0,0 +1,45 @@
+<template>
+  
+  <div>
+    <p>Here we go...</p>
+
+    <input 
+      type="file"
+      id="file-input" 
+      :v-model="fileToUpload"
+      />
+    <!--:accept="allowedContentTypes"-->
+  </div>
+  
+</template>
+
+<script lang="ts">  
+  import Vue from 'vue';
+  export default class Test extends Vue{
+    fileToUpload: File|undefined;
+
+    async put(){
+      try{
+        Vue.prototype.top = window.top;
+        let headers = new Vue.prototype.top.Headers();
+        
+        
+        headers.append("Content-Type",(fileToUpload as File).type);
+        const response = await window.top.WithIdentity.put(
+
+        )
+
+      }
+
+
+    }
+
+
+    mounted(){
+
+    }
+  }
+
+
+
+</script>
