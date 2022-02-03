bd7920b4eef4b107103b89d5eb65f2eee58ab447
TheAutomaTom <TheAutomaTom@Gmail.com>
2022-02-03 11:34:36 -0500

    make repos disposable

--- 3PA.Data.Sql/Core/Bases/PublicRecordsRepositoryBase.cs
+++ 3PA.Data.Sql/Core/Bases/PublicRecordsRepositoryBase.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,5 +25,33 @@ namespace _3PA.Data.Sql.Core.Bases
           estTimeCompleted
         );
     }
-  }
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					// TODO: dispose managed state (managed objects)
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+				// TODO: set large fields to null
+				disposedValue = true;
+			}
+		}
+
+		// // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+		// ~FlRepository()
+		// {
+		//     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		//     Dispose(disposing: false);
+		// }
+
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+	}
 }
--- 3PA.Data.Sql/Core/Interfaces/IPublicRecordsRepository.cs
+++ 3PA.Data.Sql/Core/Interfaces/IPublicRecordsRepository.cs
@@ -2,11 +2,11 @@
 
 namespace _3PA.Data.Sql.Core.Interfaces
 {
-  public interface IPublicRecordsRepository
+  public interface IPublicRecordsRepository : IDisposable
   {
+    public List<Manifest> GetManifestSummary();
     public IEnumerable<PublicRecordBase> ReadVoters(string[] list);
     public IEnumerable<PublicRecordBase> ReadHistories(string[] list);
     public Task<Manifest> CommitRecords<T>(string FileName, IEnumerable<PublicRecordBase> publicRecords) where T : class;
-
   }
 }
