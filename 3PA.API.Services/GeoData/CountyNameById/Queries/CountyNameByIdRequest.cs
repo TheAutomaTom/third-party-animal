0bc96fa9c80bec6ed3d82c0c02edd246cdcc7dd4
TheAutomaTom <TheAutomaTom@Gmail.com>
2022-02-02 21:21:56 -0500

    Refactor Get CountyNameById

---
+++ 3PA.API.Services/GeoData/CountyNameById/Queries/CountyNameByIdRequest.cs
@@ -0,0 +1,17 @@
+﻿using _3PA.Core.Models;
+using MediatR;
+
+namespace _3PA.API.Services.GeoData.CountyNameById.Queries
+{
+  public class CountyNameByIdRequest : IRequest<CountyNameByIdResponse>
+  {
+    public CountyNameByIdRequest(SupportedUsStates usState, string countyId)
+    {
+      UsState = usState;
+      CountyId = countyId;
+    }
+    public SupportedUsStates UsState { get; set; }
+    public string CountyId { get; set; }
+
+  }
+}
---
+++ 3PA.API.Services/GeoData/CountyNameById/Queries/CountyNameByIdResponse.cs
@@ -0,0 +1,18 @@
+﻿using _3PA.Core.Models;
+
+namespace _3PA.API.Services.GeoData.CountyNameById.Queries
+{
+  public class CountyNameByIdResponse
+  {
+    public CountyNameByIdResponse(CountyNameByIdRequest query, string properName = "Invalid Code")
+    {
+      UsState = query.UsState.ToString();
+      CountyId = query.CountyId;
+      ProperName = properName;
+    }
+    public string UsState { get; set; }
+    public string CountyId { get; set; }
+    public string ProperName { get; set; }
+
+  }
+}
