c84058204a0d0bbdf47d8446882a6a9b9352dc91
Thomas Grossi <TGrossi@Dealeron.com>
2022-02-04 16:22:42 -0500

    clean up

--- 3PA.UI/src/components/AppBody.vue
+++ 3PA.UI/src/components/AppBody.vue
@@ -3,14 +3,10 @@
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import AboutBody from "./About/AboutBody.vue"
-import PublicRecordsConsumerBody from "./PublicRecordsConsumer/PublicRecordsConsumerBody.vue"
 
 
 @Component({
   components: {
-    AboutBody,
-    PublicRecordsConsumerBody
   },
 })
 export default class AppBody extends Vue {
