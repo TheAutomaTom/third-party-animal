6106484db8e5aca47c376c4c20a82556d245f6db
TheAutomaTom <TheAutomaTom@Gmail.com>
2022-02-05 09:18:26 -0500

    think about linters

--- 3pa.ui/src/main.ts
+++ 3pa.ui/src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './Infrastructure/router/index'
-import { store } from './Infrastructure/store/index'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./Infrastructure/router/index";
+import { store } from "./Infrastructure/store/index";
 
 createApp(App)
 	.use(store)
 	.use(router)
-	.mount('#app')
+	.mount("#app")
